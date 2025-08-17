e579e126a382838a76dbbb9261d6c3db15db1774
lvoxx <lvoxxartist@gmail.com>
2025-08-17 14:11:17 +0700

    Fixed known issue: No delete for hotfix version when choosing the main branch of the version

--- src/features/find_lscherry/clean_linked_data.py
+++ src/features/find_lscherry/clean_linked_data.py
@@ -1,17 +1,23 @@
+import os
 import bpy  # type: ignore
 
 
-def clean_linked_libraries(version):
-    to_remove_libs = [lib for lib in bpy.data.libraries if version not in lib.filepath]
+def clean_linked_libraries(version: str):
+    target_name = f"LSCherry-{version}.blend"
+    to_remove_libs = [
+        lib
+        for lib in bpy.data.libraries
+        if os.path.basename(lib.filepath) != target_name
+    ]
     for lib in to_remove_libs:
         bpy.data.libraries.remove(lib)
 
-
 def clean_lscherry_collection(version):
+    target_name = f"LSCherry-{version}"
     to_remove_cols = [
         col
         for col in bpy.data.collections
-        if "LSCherry-" in col.name and version not in col.name
+        if col.name.startswith("LSCherry-") and col.name != target_name
     ]
     if not to_remove_cols:
         return None
