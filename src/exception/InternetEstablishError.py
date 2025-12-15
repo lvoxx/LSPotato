e837d83426765d9ae2aa96bbcc74dd0310bba7dd
lvoxx <lvoxxartist@gmail.com>
2025-12-15 10:42:43 +0700

    Removed unused errors

--- src/exception/InternetEstablishError.py
+++
@@ -1,6 +0,0 @@
-class InternetEstablishError(RuntimeError):
-    def __init__(
-        self,
-        message: str = "Can not establish internet connection. Please try again later.",
-    ):
-        super().__init__(message)
--- src/exception/SyncError.py
+++
@@ -1,6 +0,0 @@
-class UpdateLSPotatoFailedError(RuntimeError):
-    def __init__(
-        self,
-        message: str = "Can not update LSPotato. Check for lastest updates on Github.",
-    ):
-        super().__init__(message)
